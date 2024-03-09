8a66f5a365cd437dbcc23256862210b70d595a4f
Indrani Som <indranisom39@gmail.com>
2024-03-08 22:48:42 -0800

    added:added the singapore page

--- Travel/src/components/Booking/Booking.js
+++ Travel/src/components/Booking/Booking.js
@@ -1,26 +1,27 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import "./Booking.css"
-import standardRoomImage from "../../assets/japan.png";
+import standardRoomImage from "../../assets/singaporecartoon.png";
 import deluxeRoomImage from "../../assets/cuba.png";
 import suiteImage from "../../assets/paris.png";
 import bookingImage from "../../assets/circle.png";
+import { useNavigate } from "react-router-dom";
 
 const sampleCards = [
   {
-    title: "Standard Room",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    price: "$100 per night",
+    title: "Singapore",
+    description: "In Singapore, a vibrant fusion of cultures creates a tapestry of flavors, traditions, and innovation.",
+    price: "Cultures Collide, Innovation Thrives",
     image: standardRoomImage,
   },
   {
-    title: "Deluxe Room",
+    title: "India",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     price: "$150 per night",
     image: deluxeRoomImage,
   },
   {
-    title: "Suite",
+    title: "Europe",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     price: "$200 per night",
     image: suiteImage,
@@ -28,6 +29,10 @@ const sampleCards = [
 ];
 
 const Carousel = ({ cards }) => {
+  const navigate=useNavigate();
+  const gotoSingapore=()=>{
+    navigate("/singapore");
+  }
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
 
   useEffect(() => {
@@ -47,7 +52,7 @@ const Carousel = ({ cards }) => {
           <h3 className="card-title">{card.title}</h3>
           <p className="card-description">{card.description}</p>
           <p className="price">{card.price}</p>
-          <button className="book-button">Book Now</button>
+          <button className="book-button" onClick={gotoSingapore}>Know More</button>
         </div>
       ))}
     </div>
